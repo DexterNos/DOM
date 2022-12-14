857c3222a68a3bb0ef8932db2bf726846f1d3bf6
Jonathan Bautista <jon.bau.aco@outlook.com>
2022-12-14 10:49:47 -0700

    Se agregaron 8 formas de agregar elementos a lista

--- js/main.js
+++ js/main.js
@@ -6,11 +6,30 @@ console.log(elementosPorClase);
 console.log(elementosPorClase.length);
 let btn = document.getElementsByTagName("button");
 console.log(btn);
+let li = document.createElement("li"); //<li>
+li.innerText="Anothe fourth item";//<li>Anothe fourth item</li>
+li.className="list-group-item";
 
 btnEjecutar.addEventListener("click", function(event){
     event.preventDefault();
+    //console.log(li);
+    let ul = document.getElementsByTagName("ul");
+    //console.log(ul[0]);
+    //prepend primero de la lista
+    ul[0].prepend(li.cloneNode(true));
+    //append ultimo de la lista
+    ul[1].append(li.cloneNode(true));
+    // before antes del ul
+    ul[2].before(li.cloneNode(true));
+    // after despues del ul
+    ul[3].after(li.cloneNode(true));
 
-    
+
+    ul[4].insertAdjacentHTML("beforebegin", "<li class='list-group-item'>Before Begin</li>");
+    ul[4].insertAdjacentHTML("afterbegin", "<li class='list-group-item'>After Begin</li>");
+    ul[4].insertAdjacentHTML("beforeend", "<li class='list-group-item'>Before End</li>");
+    ul[4].insertAdjacentHTML("afterend", "<li class='list-group-item'>After End</li>");
+
 
 });
 
